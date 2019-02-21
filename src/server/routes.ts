a19f135da26f7d0bd2edc825eeb6c60077ecefcb
L3p1989 <phogan110@live.com>
2019-02-21 11:38:14 -0700

    added blog routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,31 @@
-import * as express from 'express';
+import * as express from "express";
+
+import DB from "./db";
 
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('World');
+router.get("/api/blogs", async (req, res) => {
+  try {
+    let blogs = await DB.blogs.all();
+    res.json(blogs);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/api/blogs/:id", async (req, res) => {
+  try {
+    let blogs = await DB.blogs.one(req.params.id);
+    res.json(blogs);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/api/hello", (req, res, next) => {
+  res.json("World");
 });
 
 export default router;
