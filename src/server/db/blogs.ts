435b1009f290935cd2ce8cce13fdfdf855d529d0
L3p1989 <phogan110@live.com>
2019-02-21 13:49:01 -0700

    altered `one`; created `insert\deleteBlog\editBlog`

--- src/server/db/blogs.ts
+++ src/server/db/blogs.ts
@@ -13,16 +13,54 @@ export const all = async () => {
 
 export const one = async (id: number) => {
   return new Promise((resolve, reject) => {
-    Connection.query("select * from blogs where id = ?", [id], (err, res) => {
-      if (err) {
-        return reject(err);
+    Connection.query(
+      `select b.*, a.name as authorid from blogs b join authors a on b.authorid = a.id where b.id = ?`,
+      [id],
+      (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(res[0]);
       }
+    );
+  });
+};
+
+export const insert = async (values: any) => {
+  return new Promise((resolve, reject) => {
+    Connection.query(`insert into Blogs set ?`, values, (err, res) => {
+      if (err) reject(err);
       resolve(res);
     });
   });
 };
 
+export const deleteBlog = async (id: number) => {
+  return new Promise((resolve, reject) => {
+    Connection.query(`delete from Blogs where id = ?`, id, (err, res) => {
+      if (err) reject(err);
+      resolve(res);
+    });
+  });
+};
+
+export const editBlog = async (values: any, id: number) => {
+  return new Promise((resolve, reject) => {
+    Connection.query(
+      `update Blogs set ${values} where id = ?`,
+      id,
+      (err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      }
+    );
+  });
+};
+
 export default {
   all,
-  one
+  one,
+  insert,
+  deleteBlog,
+  editBlog
 };
