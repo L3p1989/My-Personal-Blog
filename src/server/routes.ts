238559cb1c9ebc381c033c4063a7be9fcdab9919
L3p1989 <phogan110@live.com>
2019-02-21 13:47:45 -0700

    changed blogs to blog when grabbing 1; created post/delete/put routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -16,7 +16,17 @@ router.get("/api/blogs", async (req, res) => {
 
 router.get("/api/blogs/:id", async (req, res) => {
   try {
-    let blogs = await DB.blogs.one(req.params.id);
+    let blog = await DB.blogs.one(req.params.id);
+    res.json(blog);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/api/blogs", async (req, res) => {
+  try {
+    let blogs = await DB.blogs.insert(req.body);
     res.json(blogs);
   } catch (e) {
     console.log(e);
@@ -24,8 +34,30 @@ router.get("/api/blogs/:id", async (req, res) => {
   }
 });
 
-router.get("/api/hello", (req, res, next) => {
-  res.json("World");
+router.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    let blog = await DB.blogs.deleteBlog(req.params.id);
+    res.json(blog);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.put("/api/blogs/:id", async (req, res) => {
+  let id = req.params.id;
+  let blog = req.body;
+  try {
+    let placeholderColumns = Object.keys(blog).map(key => [
+      `${key}="${blog[key]}"`
+    ]);
+    let updateBlog = placeholderColumns.join(", ");
+    await DB.blogs.editBlog(updateBlog, id);
+    res.json("edit success!");
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 export default router;
