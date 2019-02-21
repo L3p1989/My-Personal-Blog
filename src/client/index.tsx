0f4f22183975672e6fe47f5c92cbd63d6ebae6d9
L3p1989 <phogan110@live.com>
2019-02-21 14:16:02 -0700

    corrected import for App

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import App from './App';
+import "bootstrap";
+
+import App from "./Components/App";
 
 render(<App />, document.getElementById("root"));
