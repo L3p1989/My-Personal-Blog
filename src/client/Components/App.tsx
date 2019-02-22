6d544ac08e18faff1b50e7dbd58d9b31f26fe279
L3p1989 <phogan110@live.com>
2019-02-21 18:55:23 -0700

    imported AddForm and added to "/new-blog" route

--- src/client/Components/App.tsx
+++ src/client/Components/App.tsx
@@ -7,6 +7,7 @@ import NavBar from "./NavBar";
 import HomePage from "./HomePage";
 import BlogsPage from "./BlogsPage";
 import BlogPage from "./BlogPage";
+import AddForm from "./AddForm";
 
 export default class App extends React.Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
@@ -25,6 +26,7 @@ export default class App extends React.Component<IAppProps, IAppState> {
               <Route exact path="/" component={HomePage} />
               <Route exact path="/blogs" component={BlogsPage} />
               <Route path="/blogs/:id" component={BlogPage} />
+              <Route path="/new-blog" component={AddForm} />
             </Switch>
           </>
         </Router>
