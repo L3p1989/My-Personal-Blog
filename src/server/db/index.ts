da435ac6441d63cd67ed669a7cd4844fdd7c4207
L3p1989 <phogan110@live.com>
2019-02-28 05:55:58 -0700

    corrected imports; imported/exported 'tokens'

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,20 @@
 import * as mysql from "mysql";
 import config from "../config";
 
-import blogs from "./blogs";
-import authors from "./authors";
+import blogs from "./queries/blogs";
+import authors from "./queries/authors";
+import tokens from "./queries/tokens";
 
 export const Connection = mysql.createConnection(config.mysql);
+
+export const Query = (query: string, values?: Array<string | number>) => {
+  return new Promise<Array<any>>((resolve, reject) => {
+    Connection.query(query, values, (err, results) => {
+      if (err) reject(err);
+      return resolve(results);
+    });
+  });
+};
 
 Connection.connect(err => {
   if (err) console.log(err);
@@ -12,5 +22,6 @@ Connection.connect(err => {
 
 export default {
   blogs,
-  authors
+  authors,
+  tokens
 };
