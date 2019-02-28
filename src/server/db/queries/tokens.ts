dffdc2f65c90176eb6168a5c05a4392f4621b16e
L3p1989 <phogan110@live.com>
2019-02-28 05:55:30 -0700

    created tokens with findOne/insert/update `Query`s

---
+++ src/server/db/queries/tokens.ts
@@ -0,0 +1,16 @@
+import { Query } from "../index";
+
+const findOne = async (id: string, token: string) =>
+  Query(`select * from tokens where id = '${id}' and token = '${token}'`);
+
+const insert = async (userid: number) =>
+  Query(`insert into tokens (${userid})`);
+
+const update = async (id: number, token: string) =>
+  Query(`update tokens set token = '${token}' where id = ${id}`);
+
+export default {
+  findOne,
+  insert,
+  update
+};
