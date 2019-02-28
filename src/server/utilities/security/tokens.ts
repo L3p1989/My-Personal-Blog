fbced4960ab34763a449dab4f74e5413b66541ac
L3p1989 <phogan110@live.com>
2019-02-28 10:30:39 -0700

    created tokens; created `Create/ValidToken`

---
+++ src/server/utilities/security/tokens.ts
@@ -0,0 +1,28 @@
+import * as crypto from "crypto";
+import * as jwt from "jsonwebtoken";
+
+import config from "../../config";
+import DB from "../../db";
+
+export const CreateToken = async (payload: iPayload) => {
+  let tokenid: any = await DB.tokens.insert(payload.userid);
+  payload.accesstokenid = tokenid.insertId;
+  payload.unique = crypto.randomBytes(32).toString("hex");
+  let token = await jwt.sign(payload.accesstokenid, config.auth.secret);
+  await DB.tokens.update(payload.accesstokenid, token);
+  return token;
+};
+
+export const ValidToken = async (token: string) => {
+  let payload: iPayload = <iPayload>jwt.decode(token);
+  let [accesstokenid] = await DB.tokens.findOne(payload.accesstokenid, token);
+  if (!accesstokenid) {
+    throw new Error("Invalid Token!");
+  } else return payload;
+};
+
+export interface iPayload {
+  [key: string]: any;
+  userid: number;
+  unique?: string;
+}
