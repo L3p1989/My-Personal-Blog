a14615d6d829651deebdc57186be3750a37333da
L3p1989 <phogan110@live.com>
2019-03-08 00:20:36 -0700

    added passport and strategies

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,12 @@
 import * as path from "path";
 import * as express from "express";
+import * as passport from "passport";
+
+import "./middleware/localstrategy";
+import "./middleware/bearstrategy";
+
 import apiRouter from "./routes";
+
 import * as favIcon from "serve-favicon";
 
 const app = express();
@@ -8,10 +14,10 @@ const app = express();
 app.use(favIcon(path.join(__dirname, "../public/favicon.ico")));
 
 let p = path.join(__dirname, "../public");
-console.log(p);
 
 app.use(express.static(p));
 app.use(express.json());
+app.use(passport.initialize());
 app.use(apiRouter);
 
 app.get("/*", (req, res) => {
