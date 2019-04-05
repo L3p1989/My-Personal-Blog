e86bc16c8c0a6a56be674c02bf4ddef73f86efeb
L3p1989 <phogan110@live.com>
2019-04-05 12:24:19 -0600

    removed old routes; added apiRouter

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,85 +3,11 @@ import * as express from "express";
 import DB from "../../server/db";
 
 import authRouter from "./auth";
+import apiRouter from "./api";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
-
-router.get("/api/blogs", async (req, res) => {
-  try {
-    let blogs = await DB.blogs.all();
-    res.json(blogs);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.get("/api/blogs/:id", async (req, res) => {
-  try {
-    let blog = await DB.blogs.one(req.params.id);
-    res.json(blog);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.post("/api/blogs", async (req, res) => {
-  try {
-    let blogs = await DB.blogs.insert(req.body);
-    res.json(blogs);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    let blog = await DB.blogs.deleteBlog(req.params.id);
-    res.json(blog);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.put("/api/blogs/:id", async (req, res) => {
-  let id = req.params.id;
-  let blog = req.body;
-  try {
-    let placeholderColumns = Object.keys(blog).map(key => [
-      `${key}="${blog[key]}"`
-    ]);
-    let updateBlog = placeholderColumns.join(", ");
-    await DB.blogs.editBlog(updateBlog, id);
-    res.json("edit success!");
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.get("/api/authors", async (req, res) => {
-  try {
-    let authors = await DB.authors.all();
-    res.json(authors);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.post("/api/authors", async (req, res) => {
-  try {
-    let authors = await DB.authors.insert(req.body);
-    res.json(authors);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
+router.use("/api", apiRouter);
 
 export default router;
