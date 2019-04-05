de1861625aaa7ee6afdfe078f1cb304cd51f1b33
L3p1989 <phogan110@live.com>
2019-04-05 12:09:02 -0600

    finished register.ts

--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,3 +1,26 @@
 import * as express from "express";
 
 import DB from "../../db";
+import { HashPassword } from "../../utilities/security/passwords";
+import { CreateToken } from "../../utilities/security/tokens";
+
+const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    let user = req.body;
+    user.password = HashPassword(req.body.password);
+    let result: any = await DB.authors.insert(user);
+    let token = await CreateToken({ userid: result.insertId });
+    res.json({
+      token,
+      role: "guest",
+      userid: result.insert
+    });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+export default router;
