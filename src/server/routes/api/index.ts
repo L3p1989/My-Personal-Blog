0864bd2acda43d1ae54e4ca88b79bce2b6d227a3
L3p1989 <phogan110@live.com>
2019-04-05 10:33:03 -0600

    created new index.ts under api to utilize new path

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,83 +1,17 @@
 import * as express from "express";
+import * as passport from "passport";
 
-import DB from "../../db";
+import blogsRouter from "./blogs";
 
 const router = express.Router();
 
-router.get("/api/blogs", async (req, res) => {
-  try {
-    let blogs = await DB.blogs.all();
-    res.json(blogs);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
+router.use((req, res, next) => {
+  passport.authenticate("bearer", { session: false }, (err, user, info) => {
+    if (user) req.user = user;
+    return next();
+  })(req, res, next);
 });
 
-router.get("/api/blogs/:id", async (req, res) => {
-  try {
-    let blog = await DB.blogs.one(req.params.id);
-    res.json(blog);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.post("/api/blogs", async (req, res) => {
-  try {
-    let blogs = await DB.blogs.insert(req.body);
-    res.json(blogs);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    let blog = await DB.blogs.deleteBlog(req.params.id);
-    res.json(blog);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.put("/api/blogs/:id", async (req, res) => {
-  let id = req.params.id;
-  let blog = req.body;
-  try {
-    let placeholderColumns = Object.keys(blog).map(key => [
-      `${key}="${blog[key]}"`
-    ]);
-    let updateBlog = placeholderColumns.join(", ");
-    await DB.blogs.editBlog(updateBlog, id);
-    res.json("edit success!");
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.get("/api/authors", async (req, res) => {
-  try {
-    let authors = await DB.authors.all();
-    res.json(authors);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
-
-router.post("/api/authors", async (req, res) => {
-  try {
-    let authors = await DB.authors.insert(req.body);
-    res.json(authors);
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
+router.use("/blogs", blogsRouter);
 
 export default router;
