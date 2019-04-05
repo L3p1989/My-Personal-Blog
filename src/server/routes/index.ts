6e28ad743af24e6964624a3c83578ff2ccc95a5c
L3p1989 <phogan110@live.com>
2019-04-05 10:31:48 -0600

    restored original index.ts for now

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,87 @@
 import * as express from "express";
 
+import DB from "../../server/db";
+
 import authRouter from "./auth";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+
+router.get("/api/blogs", async (req, res) => {
+  try {
+    let blogs = await DB.blogs.all();
+    res.json(blogs);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/api/blogs/:id", async (req, res) => {
+  try {
+    let blog = await DB.blogs.one(req.params.id);
+    res.json(blog);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/api/blogs", async (req, res) => {
+  try {
+    let blogs = await DB.blogs.insert(req.body);
+    res.json(blogs);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    let blog = await DB.blogs.deleteBlog(req.params.id);
+    res.json(blog);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.put("/api/blogs/:id", async (req, res) => {
+  let id = req.params.id;
+  let blog = req.body;
+  try {
+    let placeholderColumns = Object.keys(blog).map(key => [
+      `${key}="${blog[key]}"`
+    ]);
+    let updateBlog = placeholderColumns.join(", ");
+    await DB.blogs.editBlog(updateBlog, id);
+    res.json("edit success!");
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/api/authors", async (req, res) => {
+  try {
+    let authors = await DB.authors.all();
+    res.json(authors);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/api/authors", async (req, res) => {
+  try {
+    let authors = await DB.authors.insert(req.body);
+    res.json(authors);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
 
 export default router;
