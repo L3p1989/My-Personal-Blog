d6e6fbc9f51da80c718ef4d8c613947056f6c7d5
L3p1989 <phogan110@live.com>
2019-04-05 12:25:25 -0600

    added authorsRouter and path

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as passport from "passport";
 
 import blogsRouter from "./blogs";
+import authorsRouter from "./authors";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use((req, res, next) => {
 });
 
 router.use("/blogs", blogsRouter);
+router.use("/authors", authorsRouter);
 
 export default router;
