cc4af482c76b54e5395c9621cf016efe5115ca4c
L3p1989 <phogan110@live.com>
2019-04-05 12:24:39 -0600

    added old code to authors and modified to new path

--- src/server/routes/api/authors.ts
+++ src/server/routes/api/authors.ts
@@ -4,4 +4,24 @@ import DB from "../../db";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    let authors = await DB.authors.all();
+    res.json(authors);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let authors = await DB.authors.insert(req.body);
+    res.json(authors);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
 export default router;
