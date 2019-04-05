983c2f5f355c7a820bddeec18806fe4f0f1c42c8
L3p1989 <phogan110@live.com>
2019-04-05 15:02:03 -0600

    corrected payload

--- src/server/utilities/security/tokens.ts
+++ src/server/utilities/security/tokens.ts
@@ -8,13 +8,13 @@ export const CreateToken = async (payload: iPayload) => {
   let tokenid: any = await DB.tokens.insert(payload.userid);
   payload.accesstokenid = tokenid.insertId;
   payload.unique = crypto.randomBytes(32).toString("hex");
-  let token = await jwt.sign(payload.accesstokenid, config.auth.secret);
+  let token = await jwt.sign(payload, config.auth.secret);
   await DB.tokens.update(payload.accesstokenid, token);
   return token;
 };
 
 export const ValidToken = async (token: string) => {
-  let payload: iPayload = <iPayload>jwt.decode(token);
+  let payload: any = <iPayload>jwt.decode(token);
   let [accesstokenid] = await DB.tokens.findOne(payload.accesstokenid, token);
   // [ { accesstokenid } ]
   if (!accesstokenid) {
