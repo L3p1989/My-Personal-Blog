7eb63118104c8d11e15b15c5f29e0648c9f485fa
L3p1989 <phogan110@live.com>
2019-04-05 13:35:12 -0600

    corrected syntax

--- src/server/db/queries/tokens.ts
+++ src/server/db/queries/tokens.ts
@@ -4,7 +4,7 @@ const findOne = async (id: string, token: string) =>
   Query(`select * from tokens where id = '${id}' and token = '${token}'`);
 
 const insert = async (userid: number) =>
-  Query(`insert into tokens (${userid})`);
+  Query(`insert into tokens (userid) value (${userid})`);
 
 const update = async (id: number, token: string) =>
   Query(`update tokens set token = '${token}' where id = ${id}`);
