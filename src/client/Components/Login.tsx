8e98abd468873eeae4d293b2692782e7cac4e803
L3p1989 <phogan110@live.com>
2019-04-09 15:21:18 -0600

    created Login component

---
+++ src/client/Components/Login.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export default class App extends React.Component<ILoginProps, ILoginState> {
+  constructor(props: ILoginProps) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <></>;
+  }
+}
+
+interface ILoginProps {}
+
+interface ILoginState {}
