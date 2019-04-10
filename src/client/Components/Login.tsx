4b4e0dcbcbee6e9648a9c4ad4cd5bafff1fac98e
L3p1989 <phogan110@live.com>
2019-04-10 09:42:15 -0600

    added form to login

--- src/client/Components/Login.tsx
+++ src/client/Components/Login.tsx
@@ -8,7 +8,32 @@ export default class App extends React.Component<ILoginProps, ILoginState> {
   }
 
   render() {
-    return <></>;
+    return (
+      <>
+        <div className="container">
+          <form>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="User email"
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="User password"
+              />
+            </div>
+            <button className="btn my-btn">Submit</button>
+            <button className="btn my-btn">Cancel</button>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 
